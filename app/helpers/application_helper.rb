bbca5d26d19876bcd6a6caa74cf6dfda8754a496
Scott González <scott.gonzalez@gmail.com>
2019-03-08 08:16:40 -0500

    Add Open Graph meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def open_graph(options)
+    html = <<-RUBY
+      <meta property="og:title" content="#{options[:title]}">
+      <meta property="og:description" content="#{options[:description]}">
+      <link rel="image_src" href="#{options[:image_url]}">
+      <meta property="og:image" content="#{options[:image_url]}">
+      <meta property="og:image:width" content="#{options[:image_width]}">
+      <meta property="og:image:height" content="#{options[:image_height]}">
+    RUBY
+
+    content_for :open_graph, html.html_safe
+  end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,10 +1,18 @@
 class Photo < ApplicationRecord
   mount_uploader :image, ImageUploader
 
+  before_save :set_dimensions
+
   has_many :product_photos
 
   has_many :products, through: :product_photos
 
   validates :description, presence: true
   validates :image, presence: true
+
+  private
+
+  def set_dimensions
+    self.width, self.height = FastImage.size image.path
+  end
 end
